08d2d2965a2f097d98e11dff090f77044ab7a9be
clockworkyields <82741754+clockworkyields@users.noreply.github.com>
2021-04-19 00:46:50 -0400

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -4,7 +4,7 @@ import { FarmConfig } from './types'
 const farms: FarmConfig[] = [
   {
     pid: 0,
-    lpSymbol: 'CLOCK',
+    lpSymbol: 'PIANO',
     lpAddresses: {
       56: '0xd46936677B2C1Bb696F2b67c55239331E2b7Cd42',
       97: '',
@@ -14,33 +14,33 @@ const farms: FarmConfig[] = [
   },
   {
     pid: 1,
-    lpSymbol: 'CLOCK-BNB LP',
+    lpSymbol: 'PIANO-BNB LP',
     lpAddresses: {
       56: '0xb40AE531Da43F7D114F2E6D9c331171Ef4130f5F',
       97: '',
     },
-    token: tokens.clock,
+    token: tokens.piano,
     quoteToken: tokens.wbnb,
   },
   {
     pid: 2,
-    lpSymbol: 'CLOCK-BUSD LP',
+    lpSymbol: 'PIANO-BUSD LP',
     lpAddresses: {
       56: '0xd8D201CeBC25c39AD3E05536F94ECc3B6Bc8cAcc',
       97: '',
     },
-    token: tokens.clock,
+    token: tokens.piano,
     quoteToken: tokens.busd,
   },
   {
     pid: 3,
-    lpSymbol: 'BTC-BNB LP',
+    lpSymbol: 'PIANO-CAKE LP',
     lpAddresses: {
       56: '0x0E7197386A37c0D792D9D357D59f89b679cf1b06',
       97: '',
     },
-    token: tokens.btc,
-    quoteToken: tokens.wbnb,
+    token: tokens.piano,
+    quoteToken: tokens.cake,
   },
   {
     pid: 4,
@@ -54,13 +54,13 @@ const farms: FarmConfig[] = [
   },
   {
     pid: 5,
-    lpSymbol: 'FUEL-BNB LP',
+    lpSymbol: 'USDC-BUSD LP',
     lpAddresses: {
       56: '0x680dd100e4b394bda26a59dd5c119a391e747d18',
       97: ''
     },
-    token: tokens.fuel,
-    quoteToken: tokens.wbnb,
+    token: tokens.usdc,
+    quoteToken: tokens.busd,
   },
   {
     pid: 6,
@@ -74,43 +74,43 @@ const farms: FarmConfig[] = [
   },
   {
     pid: 8,
-    lpSymbol: 'CLOCK-ALLOY LP',
+    lpSymbol: 'PIANO-ADA LP',
     lpAddresses: {
       56: '0xfFeb22c6143B982699E2dcBafBb0EAC51d0F408E',
       97: ''
     },
-    token: tokens.clock, 
-    quoteToken: tokens.alloy,
+    token: tokens.piano,
+    quoteToken: tokens.ada,
   },
   {
     pid: 9,
-    lpSymbol: 'CLOCK-CAKE LP',
+    lpSymbol: 'PIANO-DOT LP',
     lpAddresses: {
       56: '0xe3905652c879d9df7319b5756350dd2bb5e052e9',
       97: ''
     },
-    token: tokens.clock,
-    quoteToken: tokens.cake,
+    token: tokens.piano,
+    quoteToken: tokens.dot,
   },
   {
     pid: 10,
-    lpSymbol: 'HYPR-BNB LP',
+    lpSymbol: 'USDT-BUSD LP',
     lpAddresses: {
       56: '0xc15fa3e22c912a276550f3e5fe3b0deb87b55acd',
       97: ''
     },
-    token: tokens.hypr,
-    quoteToken: tokens.wbnb,
+    token: tokens.usdt,
+    quoteToken: tokens.busd,
   },
   {
     pid: 11,
-    lpSymbol: 'CDMC-BNB LP',
+    lpSymbol: 'DOT-ETH LP',
     lpAddresses: {
       56: '0x84cd882a0abaf8ad69b85fef0a8d5a36f5c0ecc1',
       97: ''
     },
-    token: tokens.cdmc,
-    quoteToken: tokens.wbnb,
+    token: tokens.dot,
+    quoteToken: tokens.eth,
   },
 
 
