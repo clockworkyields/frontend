cf26cb74844a0cc5afa71e04b37b66e5a9e999f4
anon <cockswapDev@protonmail.com>
2021-06-10 21:37:18 -0400

    rm icon

--- src/views/Pools/components/Coming.tsx
+++ src/views/Pools/components/Coming.tsx
@@ -65,7 +65,7 @@ const Coming: React.FC = () => {
         <DetailPlaceholder>
           <div style={{ flex: 1 }}>
             <span role="img" aria-label="syrup">
-              ❄️{' '}
+              {' '}
             </span>
             {TranslateString(384, 'Your Stake')}:
           </div>
