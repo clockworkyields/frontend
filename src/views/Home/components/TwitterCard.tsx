e073afa423312e3f206a234d0225ce8ca9dbaff7
anon <cockswapDev@protonmail.com>
2021-06-12 12:19:25 -0400

    link

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'Blizzard.money',
+            screenName: 'https://clockwork-yields.medium.com/',
           }}
           options={{
             height: '300',
