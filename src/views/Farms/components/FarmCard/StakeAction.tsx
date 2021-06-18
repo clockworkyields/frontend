6380946f24511c28959d4a6b771f51982efb076f
clockworkyields <82741754+clockworkyields@users.noreply.github.com>
2021-06-18 00:18:36 -0400

    Update StakeAction.tsx

--- src/views/Farms/components/FarmCard/StakeAction.tsx
+++ src/views/Farms/components/FarmCard/StakeAction.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 import BigNumber from 'bignumber.js'
 import { Button, Flex, Heading, useModal } from 'clock-uikit'
-// import useI18n from 'hooks/useI18n'
-// import useStake from 'hooks/useStake'
+import useI18n from 'hooks/useI18n'
+import useStake from 'hooks/useStake'
 import useUnstake from 'hooks/useUnstake'
 import { getBalanceNumber } from 'utils/formatBalance'
-// import DepositModal from '../DepositModal'
+import DepositModal from '../DepositModal'
 import WithdrawModal from '../WithdrawModal'
 
 interface FarmCardActionsProps {
@@ -17,44 +17,44 @@ interface FarmCardActionsProps {
   depositFeeBP?: number
 }
 
-// const IconButtonWrapper = styled.div`
-//   display: flex;
-//   svg {
-//     width: 20px;
-//   }
-// `
+const IconButtonWrapper = styled.div`
+  display: flex;
+  svg {
+    width: 20px;
+  }
+`
 
 const StakeAction: React.FC<FarmCardActionsProps> = ({ stakedBalance, tokenName, pid }) => {
-  // const TranslateString = useI18n()
-  // const { onStake } = useStake(pid)
+const TranslateString = useI18n()
+const { onStake } = useStake(pid)
   const { onUnstake } = useUnstake(pid)
 
   const rawStakedBalance = getBalanceNumber(stakedBalance)
   const displayBalance = rawStakedBalance.toLocaleString()
 
-  // const [onPresentDeposit] = useModal(
-  //   <DepositModal max={tokenBalance} onConfirm={onStake} tokenName={tokenName} depositFeeBP={depositFeeBP} />,
-  // )
+   const [onPresentDeposit] = useModal(
+     <DepositModal max={tokenBalance} onConfirm={onStake} tokenName={tokenName} depositFeeBP={depositFeeBP} />,
+   )
   const [onPresentWithdraw] = useModal(
     <WithdrawModal max={stakedBalance} onConfirm={onUnstake} tokenName={tokenName} />,
   )
 
-  // const renderStakingButtons = () => {
-  //   return rawStakedBalance === 0 ? (
-  //     <Button style={{ width: 107.72 }} onClick={onPresentDeposit}>
-  //       {TranslateString(999, 'Stake')}
-  //     </Button>
-  //   ) : (
-  //     <IconButtonWrapper>
-  //       <IconButton style={{ width: 52 }} variant="tertiary" onClick={onPresentWithdraw} mr="6px">
-  //         <MinusIcon color="primary" />
-  //       </IconButton>
-  //       <IconButton style={{ width: 52 }} variant="tertiary" onClick={onPresentDeposit}>
-  //         <AddIcon color="primary" />
-  //       </IconButton>
-  //     </IconButtonWrapper>
-  //   )
-  // }
+   const renderStakingButtons = () => {
+     return rawStakedBalance === 0 ? (
+       <Button style={{ width: 107.72 }} onClick={onPresentDeposit}>
+         {TranslateString(999, 'Stake')}
+       </Button>
+     ) : (
+       <IconButtonWrapper>
+         <IconButton style={{ width: 52 }} variant="tertiary" onClick={onPresentWithdraw} mr="6px">
+           <MinusIcon color="primary" />
+         </IconButton>
+         <IconButton style={{ width: 52 }} variant="tertiary" onClick={onPresentDeposit}>
+           <AddIcon color="primary" />
+         </IconButton>
+       </IconButtonWrapper>
+     )
+   }
 
   const renderUnstakingButtons = () => {
     return (
