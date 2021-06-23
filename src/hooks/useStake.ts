180a31264160ceabe9771497aa8ce68028c1572d
clockworkyields <82741754+clockworkyields@users.noreply.github.com>
2021-06-23 10:21:02 -0400

    Update useStake.ts

--- src/hooks/useStake.ts
+++ src/hooks/useStake.ts
@@ -17,7 +17,7 @@ const useStake = (pid: number) => {
       else {
       const txHash = await stake(masterChefContract, pid, amount, account)}
       dispatch(fetchFarmUserDataAsync(account))
-      console.info(txHash)
+
     },
     [account, dispatch, masterChefContract, pid],
   )
