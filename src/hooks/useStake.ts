03bff02a48c9832984b28319ea7ee02a07def6b8
clockworkyields <82741754+clockworkyields@users.noreply.github.com>
2021-06-23 09:54:48 -0400

    Update useStake.ts

--- src/hooks/useStake.ts
+++ src/hooks/useStake.ts
@@ -2,7 +2,7 @@ import { useCallback } from 'react'
 import { useWallet } from '@binance-chain/bsc-use-wallet'
 import { useDispatch } from 'react-redux'
 import { fetchFarmUserDataAsync, updateUserStakedBalance, updateUserBalance } from 'state/actions'
-import { stake, sousStake, sousStakeBnb } from 'utils/callHelpers'
+import { stake, sousStake, sousStakeBnb, enterstaking } from 'utils/callHelpers'
 import { useMasterchef, useSousChef } from './useContract'
 
 const useStake = (pid: number) => {
@@ -12,6 +12,9 @@ const useStake = (pid: number) => {
 
   const handleStake = useCallback(
     async (amount: string) => {
+      if (pid === 0) {
+        const txHash = await enterstaking(masterchefcontract, amount, account)
+      else
       const txHash = await stake(masterChefContract, pid, amount, account)
       dispatch(fetchFarmUserDataAsync(account))
       console.info(txHash)
